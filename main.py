4e73e43265a24f153328e6f2946e9cf89bc57a85
domssilva <domenique_silva@hotmail.com>
2023-09-20 21:35:30 +0200

    feature: get IMC params from user input

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-def get_imc():
+def get_imc(altura, peso):
     print('todo')
+
+altura = input('Insira a sua altura (exemplo - 1.70, 1.56, 1.82): ')
+peso = input('Insira o seu peso: ')
+
+imc = get_imc(altura, peso)
+print(f'IMC = {imc}')
