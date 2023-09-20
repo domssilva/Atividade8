5a96df78a65fe8e056fbe0f493f787160a305947
domssilva <domenique_silva@hotmail.com>
2023-09-20 21:35:45 +0200

    fix: type casting

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def get_imc(altura, peso):
-    print('todo')
+# Function that calculates a person's IMC
+def get_imc(altura: float, peso: float):
+    return peso / (altura ** 2)
 
-altura = input('Insira a sua altura (exemplo - 1.70, 1.56, 1.82): ')
-peso = input('Insira o seu peso: ')
+altura = float(input('Insira a sua altura (exemplo - 1.70, 1.56, 1.82): '))
+peso = float(input('Insira o seu peso: '))
 
 imc = get_imc(altura, peso)
 print(f'IMC = {imc}')
